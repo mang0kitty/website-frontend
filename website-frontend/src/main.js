90e5f365d3c51af4a564ce42b613ad0b4b91ce49
Aideen Fay <me@aideen.dev>
2020-06-18 13:38:11 +0100

    add HTML stripping

--- website-frontend/src/main.js
+++ website-frontend/src/main.js
@@ -7,6 +7,13 @@ import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
+Vue.filter("striphtml", function(value) {
+  var div = document.createElement("div");
+  div.innerHTML = value;
+  var text = div.textContent || div.innerText || "";
+  return text;
+});
+
 new Vue({
   router: Router,
   render: (h) => h(App),
