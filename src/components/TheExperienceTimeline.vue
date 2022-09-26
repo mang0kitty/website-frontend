6e5d9b530c54c854ec4332f2ce92f4024f2db0fa
Aideen Fay <me@aideen.dev>
2022-09-26 21:18:57 +0100

    update: update my work experience notes

--- src/components/TheExperienceTimeline.vue
+++ src/components/TheExperienceTimeline.vue
@@ -16,14 +16,14 @@ export default {
         {
           company: "Microsoft",
           role: "Security Software Engineer II",
-          description: "At Microsoft I'm a Threat Hunter on the MSRC Threat Hunting team where our objective is to proactively find previously undetected threats in Azure first-party infrastructure - that is the underlying infrastructure that runs Azure services for our customers.",
+          description: "At Microsoft I'm a Threat Hunter on the Cloud and AI Threat Hunting team where our objective is to proactively identify and report on security gaps in Azure infrastructure that would allow an adversary to operate undetected. In addition to my core technical responsibilities, I have also supported team management in making sure our team's execution and mission is aligned with the organization's broader objectives.",
           date: "November 2021 - Present"
         },
         {
           company: "Amazon",
           role: "Security Engineer II",
           description:
-            "At Amazon, I conducted defensive security engineering for Amazon's subsidiaries and acquisitions. This included the development of automated tools for restricted environment management, and the maintenance and use of large data query platforms.",
+            "I worked on Amazon’s Subsidiary Security team, which was responsible for M&A due diligence and the security integration of subsidiaries into Amazon’s tech and cultural ecosystem. I worked directly with the product and security teams to assess the probability of a risk event to Amazon and address security and visibility gaps across their cloud and on-prem environments.",
           date: "July 2020 - November 2021",
         },
         {
